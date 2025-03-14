11a244e2c2cb07ba8d1da9f6d1c361e29b4fd9ef
smcoderr <souravoz2018@gmail.com>
2025-03-14 20:04:55 +0530

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function AboutPage() {
+    console.log("hey form about page")
+  return (
+    <div>Hello from AboutPage</div>
+  )
+}
+
+export default AboutPage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        navbar
         {children}
+        footer
       </body>
     </html>
   );
