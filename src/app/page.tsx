33acedb159ae3378be566aca5de82912bf9e5cd9
smcoderr <souravoz2018@gmail.com>
2025-03-15 10:32:53 +0530

    clerk and shadcn installed, dark and light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Home Page</h1>
+    <div className="m-4">
+      <SignedOut>
+              <SignInButton mode="modal">
+                <Button>Sign in</Button>
+              </SignInButton>
+              {/* <SignUpButton /> */}
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+
+            <ModeToggle />
+            <Button variant={"secondary"}>Click me</Button>
     </div>
   );
 }
