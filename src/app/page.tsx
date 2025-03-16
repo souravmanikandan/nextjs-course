c7d083b7a88c6e64494d79428746b923e035f421
smcoderr <souravoz2018@gmail.com>
2025-03-16 19:41:45 +0530

    removed unwanted imported

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>Home  page content</h1>
