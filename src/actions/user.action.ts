a22b53d3a007e3d88d6b853ba1e0149d4f135b8e
smcoderr <souravoz2018@gmail.com>
2025-03-17 17:20:21 +0530

    idk

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -22,12 +22,12 @@ export async function syncUser() {
       data: {
         clerkId: userId,
         name: `${user.firstName || ""} ${user.lastName || ""}`,
-        username:
-          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
         email: user.emailAddresses[0].emailAddress,
         image: user.imageUrl,
       },
     });
+
     return dbUser;
   } catch (error) {
     console.log("Error in syncUser", error);
